b2a3af263548fd5de4f1a97ce0ec950bdb7fd912
juanFGR <juan.ramos.fg1@gmail.com>
2015-09-19 00:56:38 +0100

    test

--- app.js
+++ app.js
@@ -24,13 +24,13 @@ client.get('statuses/user_timeline', params, function(error, tweets, response){
 });
 
 client.get('favorites/list', function(error, tweets, response){
-  if(error) throw error;
+  //if(error) throw error;
   console.log(tweets);  // The favorites. 
   console.log(response);  // Raw response object. 
 });
 
 client.post('statuses/update', {status: 'I Love Twitter'},  function(error, tweet, response){
-  if(error) throw error;
+  //if(error) throw error;
   console.log(tweet);  // Tweet body. 
   console.log(response);  // Raw response object. 
 });
