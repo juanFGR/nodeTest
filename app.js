15d6fd9aafecb11c9715556b6d38dc66451a835f
juanFGR <juan.ramos.fg1@gmail.com>
2015-09-18 18:53:49 +0100

    test

--- app.js
+++ app.js
@@ -27,12 +27,9 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-app.set('port', (process.env.PORT || 5000));
+// Routes
+app.get('/', routes.index);
 
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
