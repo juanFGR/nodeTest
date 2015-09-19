8c288e6120b3f0c15812cfe2ead452077cff1d5b
Juan_FGR <juan.ramos.fg1@gmail.com>
2015-09-19 19:05:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 var config = {
 	twitter:{
-		key:'ouy8sE5HZvOHuBVBOZAN0WgJY',
-		secret:'Y9AXQdYTWw6rnLYmPvMwNrFDADGC80g2z6aLZ3SCaK2HuZG8zj'
+		key:'Consumer Key (API Key)',
+		secret:'Consumer Secret (API Secret)'
 	},
 	facebook:{
-		key:'682459788477116',
-		secret:'b931a741f3efae0d84366545c2e1427e'
+		key:'Consumer Key (API Key)',
+		secret:'Consumer Secret (API Secret)'
 	}
 
 	
