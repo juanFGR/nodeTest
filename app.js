47e371bfb5a98df8616fd4db229a342021bb7940
juanFGR <juan.ramos.fg1@gmail.com>
2015-09-19 01:43:49 +0100

    test

--- app.js
+++ app.js
@@ -3,108 +3,86 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
 
-var app = module.exports = express.createServer();
+var express = require('express'); // Express: Framework HTTP para Node.js
+var routes = require('./routes'); // Dónde tenemos la configuración de las rutas
+var path = require('path');
 
+var mongoose = require('mongoose'); // Mongoose: Libreria para conectar con MongoDB
+var passport = require('passport'); // Passport: Middleware de Node que facilita la autenticación de usuarios
 
-var Twitter = require('twitter');
-var client = new Twitter({
-    consumerKey: 'ouy8sE5HZvOHuBVBOZAN0WgJY',
-    consumerSecret: 'Y9AXQdYTWw6rnLYmPvMwNrFDADGC80g2z6aLZ3SCaK2HuZG8zj',
-    access_token_key: '374141615-2xSdReXe9GlsjNl8jlbUA9cfbhpTAMtwP8PRVwtC',
-    access_token_secret: 'CK6MawyTAKBU94ZCB27iBrx4TitMcxtPjbpGCdjxBzqW2'
+// Importamos el modelo usuario y la configuración de passport
+require('./models/user');
+require('./passport')(passport);
+
+// Conexión a la base de datos de MongoDB que tenemos en local
+mongoose.connect('mongodb://localhost:27017/passport-example', function(err, res) {
+  if(err) throw err;
+  console.log('Conectado con éxito a la BD');
 });
 
-// Configuration
+// Iniciamos la aplicación Express
+var app = express();
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+// Configuración (Puerto de escucha, sistema de plantillas, directorio de vistas,...)
+app.set('port', process.env.PORT || 5000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
 
-var OAuth = require('oauth').OAuth
-  , oauth = new OAuth(
-      "https://api.twitter.com/oauth/request_token",
-      "https://api.twitter.com/oauth/access_token",
-      "ouy8sE5HZvOHuBVBOZAN0WgJY",
-      "Y9AXQdYTWw6rnLYmPvMwNrFDADGC80g2z6aLZ3SCaK2HuZG8zj",
-      "1.0",
-      "https://immense-chamber-2876.herokuapp.com/auth/twitter/callback",
-      "HMAC-SHA1"
-    );
+// Middlewares de Express que nos permiten enrutar y poder
+// realizar peticiones HTTP (GET, POST, PUT, DELETE)
+app.use(express.cookieParser());
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.methodOverride());
 
+// Ruta de los archivos estáticos (HTML estáticos, JS, CSS,...)
+app.use(express.static(path.join(__dirname, 'public')));
+// Indicamos que use sesiones, para almacenar el objeto usuario
+// y que lo recuerde aunque abandonemos la página
+app.use(express.session({ secret: 'lollllo' }));
 
-app.get('/auth/twitter', function(req, res) {
- 
-  oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
-    if (error) {
-      console.log(error);
-      res.send("Authentication Failed!");
-    }
-    else {
-      req.session.oauth = {
-        token: oauth_token,
-        token_secret: oauth_token_secret
-      };
-      console.log(req.session.oauth);
-      res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
-    }
-  });
- 
-});
+// Configuración de Passport. Lo inicializamos
+// y le indicamos que Passport maneje la Sesión
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
 
-app.get('/auth/twitter/callback', function(req, res, next) {
- 
-  if (req.session.oauth) {
-    req.session.oauth.verifier = req.query.oauth_verifier;
-    var oauth_data = req.session.oauth;
- 
-    oauth.getOAuthAccessToken(
-      oauth_data.token,
-      oauth_data.token_secret,
-      oauth_data.verifier,
-      function(error, oauth_access_token, oauth_access_token_secret, results) {
-        if (error) {
-          console.log(error);
-          res.send("Authentication Failure!");
-        }
-        else {
-          req.session.oauth.access_token = oauth_access_token;
-          req.session.oauth.access_token_secret = oauth_access_token_secret;
-          console.log(results, req.session.oauth);
-          res.send("Authentication Successful");
-          // res.redirect('/'); // You might actually want to redirect!
-        }
-      }
-    );
-  }
-  else {
-    res.redirect('/login'); // Redirect to login page
-  }
- 
-});
-
-
-
-
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
+// Si estoy en local, le indicamos que maneje los errores
+// y nos muestre un log más detallado
+if ('development' == app.get('env')) {
   app.use(express.errorHandler());
-});
+}
 
-// Routes
+/* Rutas de la aplicación */
+// Cuando estemos en http://localhost:puerto/ (la raiz) se ejecuta el metodo index
+// del modulo 'routes'
 app.get('/', routes.index);
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+/* Rutas de Passport */
+// Ruta para desloguearse
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+// Ruta para autenticarse con Twitter (enlace de login)
+app.get('/auth/twitter', passport.authenticate('twitter'));
+// Ruta para autenticarse con Facebook (enlace de login)
+app.get('/auth/facebook', passport.authenticate('facebook'));
+// Ruta de callback, a la que redirigirá tras autenticarse con Twitter.
+// En caso de fallo redirige a otra vista '/login'
+app.get('/auth/twitter/callback', passport.authenticate('twitter',
+  { successRedirect: '/', failureRedirect: '/login' }
+));
+// Ruta de callback, a la que redirigirá tras autenticarse con Facebook.
+// En caso de fallo redirige a otra vista '/login'
+app.get('/auth/facebook/callback', passport.authenticate('facebook',
+  { successRedirect: '/', failureRedirect: '/login' }
+));
+
+// Inicio del servidor
+app.listen(app.get('port'), function(){
+  console.log('Aplicación Express escuchando en el puerto ' + app.get('port'));
 });
---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {
+	twitter:{
+		key:'ouy8sE5HZvOHuBVBOZAN0WgJY',
+		secret:'Y9AXQdYTWw6rnLYmPvMwNrFDADGC80g2z6aLZ3SCaK2HuZG8zj'
+	}
+}
+module.exports = config;
