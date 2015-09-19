a3300e70cb907c7677bb811f8acaae5cf44d94a0
juanFGR <juan.ramos.fg1@gmail.com>
2015-09-19 02:23:33 +0100

    test

--- config.js
+++ config.js
@@ -2,6 +2,12 @@ var config = {
 	twitter:{
 		key:'ouy8sE5HZvOHuBVBOZAN0WgJY',
 		secret:'Y9AXQdYTWw6rnLYmPvMwNrFDADGC80g2z6aLZ3SCaK2HuZG8zj'
+	},
+	facebook:{
+		key:'682459788477116',
+		secret:'b931a741f3efae0d84366545c2e1427e'
 	}
+
+	
 }
 module.exports = config;
